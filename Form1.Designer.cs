d54da9b213dac7cb3b8903d7b5968ad30535630d
passhinking <983149482@qq.com>
2021-08-12 15:59:59 +0800

    连接上ServiceSocket

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,46 +31,46 @@ namespace SocketClent
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PorttextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.IPtextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timerSocketSend = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(339, 25);
+            this.button1.Location = new System.Drawing.Point(417, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "连接Socket";
+            this.button1.Text = "开始连接";
             this.button1.UseVisualStyleBackColor = true;
- 
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // PorttextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(275, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(58, 21);
-            this.textBox1.TabIndex = 1;
-           
+            this.PorttextBox.Location = new System.Drawing.Point(339, 25);
+            this.PorttextBox.Name = "PorttextBox";
+            this.PorttextBox.Size = new System.Drawing.Size(58, 21);
+            this.PorttextBox.TabIndex = 1;
+            this.PorttextBox.Text = "55550";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(217, 30);
+            this.label1.Location = new System.Drawing.Point(280, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "端口号：";
-       
             // 
             // label2
             // 
@@ -81,20 +81,13 @@ namespace SocketClent
             this.label2.TabIndex = 4;
             this.label2.Text = "IP地址：";
             // 
-            // textBox2
+            // IPtextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(70, 25);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(133, 21);
-            this.textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(14, 94);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(400, 417);
-            this.textBox3.TabIndex = 5;
+            this.IPtextBox.Location = new System.Drawing.Point(70, 25);
+            this.IPtextBox.Name = "IPtextBox";
+            this.IPtextBox.Size = new System.Drawing.Size(204, 21);
+            this.IPtextBox.TabIndex = 3;
+            this.IPtextBox.Text = "www.passthinking.com";
             // 
             // label3
             // 
@@ -113,62 +106,105 @@ namespace SocketClent
             this.label4.Size = new System.Drawing.Size(77, 12);
             this.label4.TabIndex = 8;
             this.label4.Text = "发送的数据：";
-
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(438, 94);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(467, 417);
-            this.textBox4.TabIndex = 7;
-            
             // 
             // button2
             // 
+            this.button2.Enabled = false;
             this.button2.Location = new System.Drawing.Point(821, 65);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 9;
             this.button2.Text = "发送数据";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "清除接收数据";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.textBox3.Location = new System.Drawing.Point(14, 97);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(400, 414);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.textBox4.Location = new System.Drawing.Point(438, 97);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(458, 414);
+            this.textBox4.TabIndex = 12;
+            // 
+            // timerSocketSend
+            // 
+            this.timerSocketSend.Tick += new System.EventHandler(this.timerSocketSend_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(812, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "使能心跳包";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(917, 523);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.IPtextBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PorttextBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
+       
+
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox PorttextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox IPtextBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timerSocketSend;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,12 +13,234 @@ namespace SocketClent
 {
     public partial class Form1 : Form
     {
-        
+        private Socket clientSocket = null;
+        private Thread ReceiveThread = null;
+        private bool ReceiveFlag = false;
+
         public Form1()
         {
             InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+            timerSocketSend.Interval = 60*1000;
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("开始连接"))
+            {
+                button1.Enabled = false;
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Thread t = new Thread(new ThreadStart(ConnectServerSocket));
+                t.Start();
+            }
+            else 
+            {
+                clientSocket.Close();
+                button1.Text = "开始连接";
+                button2.Enabled = false;
+                ReceiveFlag = false;
+                timerSocketSend.Stop();
+                clientSocket = null;
+            }
             
         }
 
+
+         void ConnectServerSocket()
+        {
+            this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+            {
+                textBox3.AppendText("开始获取ip... " + Environment.NewLine);
+            }));
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(IPtextBox.Text);
+            IPAddress[] iPAddresses = iPHostEntry.AddressList;
+            foreach (IPAddress ip in iPAddresses)
+            {
+                Console.WriteLine(ip);
+            }
+            if (iPAddresses.Length > 0)
+            {
+                this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                {
+                    textBox3.AppendText("IP:" + iPAddresses[0] + "  获取成功！" + Environment.NewLine);
+                }));
+                try
+                {
+                    int port = 0;
+                    int.TryParse(PorttextBox.Text,out port);
+                    clientSocket.Connect(new IPEndPoint(iPAddresses[0], port));
+                    Console.WriteLine("连接到服务器成功！\n");
+                    
+
+                    this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                    {
+                        textBox3.AppendText("连接成功！" + Environment.NewLine);
+                    }));
+                    this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                    {
+                        button1.Text = "断开连接";
+                        button1.Enabled = true;
+                        button2.Enabled = true;
+                    }));
+
+                   
+
+                    ReceiveFlag = true;
+                    ReceiveThread = new Thread(new ThreadStart(receiverLoop));
+                    ReceiveThread.Start();
+                    if (checkBox1.Checked)
+                    {
+                        this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                        {
+
+                            timerSocketSend.Start();
+                        }));
+                    }
+                      
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("连接到服务器失败！");
+                    MessageBox.Show("连接失败！");
+                    this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                    {
+                        button1.Enabled = true;
+                    }));
+                    return;
+                }
+            }
+        }
+
+
+        void receiverLoop()
+        {
+            byte[] rbyte = new byte[1024 * 4];
+            int rlength = 0;
+            StringBuilder sReceive = new StringBuilder();
+            Console.WriteLine("socket 超时时间：" + clientSocket.SendTimeout);
+            while (ReceiveFlag)
+            {
+                try
+                {
+                    rlength = clientSocket.Receive(rbyte);
+                    if (rlength > 0)
+                    {
+                        byte[] r = new byte[rlength];
+                        Array.Copy(rbyte, r, rlength);
+                        this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                        {
+                            textBox3.Text = textBox3.Text +  Encoding.UTF8.GetString(r).Replace("\n", Environment.NewLine);
+                            textBox3.Focus();
+                      
+                        }));
+                        Console.WriteLine(Encoding.UTF8.GetString(r));
+                    }
+                    else
+                    {
+                        this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                        {
+                            if (clientSocket != null)
+                            {
+                                clientSocket.Close();
+                            }
+                            button1.Text = "开始连接";
+                            button2.Enabled = false;
+                            ReceiveFlag = false;
+                        }));
+                        return;
+                    }
+                }
+                catch
+                {
+                    this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+                    {
+                        if (clientSocket != null)
+                        {
+                            clientSocket.Close();
+                        }
+                        button1.Text = "开始连接";
+                        button2.Enabled = false;
+                        ReceiveFlag = false;
+                    }));
+                    return;
+                }
+                
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.BeginInvoke(new System.Threading.ThreadStart(delegate ()
+            {
+                textBox3.Text = "";
+            }));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length > 0)
+            {
+                byte[] t = Encoding.UTF8.GetBytes(textBox4.Text);
+                if (clientSocket != null)
+                {
+                    clientSocket.Send(t);
+                }
+                
+            }
+            Console.WriteLine(checkBox1.Checked);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+           
+            if (clientSocket != null)
+            {
+                clientSocket.Close();
+            }
+            timerSocketSend.Stop();
+            ReceiveFlag = false;
+            clientSocket = null;
+            Console.WriteLine("关闭");
+        }
+
+
+        private void timerSocketSend_Tick(object sender, EventArgs e)
+        {
+
+            if(clientSocket != null)
+            {
+                clientSocket.Send(Encoding.UTF8.GetBytes("I live new!"+ Environment.NewLine));
+                Console.WriteLine("发送心跳包！");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(checkBox1.Checked);
+            
+            if (clientSocket != null)
+            {
+                if (checkBox1.Checked)
+                {
+                    timerSocketSend.Start();
+                }
+                else
+                {
+                    timerSocketSend.Stop();
+                }
+                
+            }
+        }
     }
 }
